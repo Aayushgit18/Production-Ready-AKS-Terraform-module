342d0c0dee8784157e167d74f2db1012b1b15a8f
Aayushgit18 <aayushchoundiye@gmail.com>
2026-01-29 11:58:05 +0530

    Update backend.tf

--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "tfstate-rg"
-    storage_account_name = "aksstatefile"
+    storage_account_name = " storage_account_name "
     container_name       = "tfstate"
     key                  = "prod/aks.tfstate"
   }
