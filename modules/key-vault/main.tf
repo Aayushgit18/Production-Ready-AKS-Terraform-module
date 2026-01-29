cd06299036adbfce1443fac71af0e92cf5087060
Ayush Choundiye <ayush.choundiye@nitorinfotech.com>
2026-01-29 17:02:19 +0530

    done

--- modules/key-vault/main.tf
+++ modules/key-vault/main.tf
@@ -1,50 +1,35 @@
-############################################
-# Azure Key Vault
-############################################
-
 resource "azurerm_key_vault" "this" {
-  name                        = var.name
-  location                    = var.location
-  resource_group_name         = var.rg_name
-  tenant_id                   = var.tenant_id
-
-  # Standard SKU is recommended for most workloads
-  sku_name = "standard"
-
-  # IMPORTANT:
-  # We enable RBAC instead of access policies.
-  # This is required for Workload Identity + CSI Driver.
-  enable_rbac_authorization = true
-
-  # Best practice: do not expose Key Vault publicly
+  name                          = var.name
+  location                      = var.location
+  resource_group_name           = var.rg_name
+  tenant_id                     = var.tenant_id
+  sku_name                      = "standard"
+  enable_rbac_authorization     = true
   public_network_access_enabled = false
-
-  tags = var.tags
+  tags                          = var.tags
 }
-
-############################################
-# User Assigned Managed Identity
-# 1️⃣ Backend Identity
-############################################
 
 resource "azurerm_user_assigned_identity" "backend" {
-  # Azure-visible name of the identity
-  name = "uami-backend-${var.name}"
-
+  name                = "uami-backend-${var.name}"
   location            = var.location
   resource_group_name = var.rg_name
 }
 
-############################################
-# User Assigned Managed Identity
-# 2️⃣ MySQL Identity
-############################################
-
 resource "azurerm_user_assigned_identity" "mysql" {
-  # Azure-visible name of the identity
-  name = "uami-mysql-${var.name}"
-
+  name                = "uami-mysql-${var.name}"
   location            = var.location
   resource_group_name = var.rg_name
+}
+
+resource "azurerm_role_assignment" "backend_kv_secrets_user" {
+  scope                = azurerm_key_vault.this.id
+  role_definition_name = "Key Vault Secrets User"
+  principal_id         = azurerm_user_assigned_identity.backend.principal_id
+}
+
+resource "azurerm_role_assignment" "mysql_kv_secrets_user" {
+  scope                = azurerm_key_vault.this.id
+  role_definition_name = "Key Vault Secrets User"
+  principal_id         = azurerm_user_assigned_identity.mysql.principal_id
 }
 
